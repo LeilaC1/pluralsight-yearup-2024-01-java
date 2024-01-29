2387849768f4cd779b2b610ff0a6e74cb81a62b0
LeilaC1 <leilaclesca13@gmail.com>
2024-01-29 15:59:20 -0500

    class demo

---
+++ week 1/LanguagesDemo/src/MainApplication.java
@@ -0,0 +1,15 @@
+public class MainApplication {
+// public static void main = entry point of program
+   public static void main(String[]args){
+       System.out.println("HTML");
+       System.out.println("CSS");
+
+       // declare a variable
+       String name = "Leila";
+       char firstInitial = 'L';
+       int age = 18;
+
+   }
+
+
+}
---
+++ week 1/com.pluralsight/src/MathApp.java
@@ -0,0 +1,16 @@
+public class MathApp {
+        public static void main(String[] args) {
+// Question 1 self practice ex. :
+            // declare variables here
+int num1 = 2;
+int num2 = 5;
+// then code solution
+            int sum = num1 + num2;
+            // then use System.out.println() to display results
+            // ex: System.out.println("The answer is " + answer);
+            System.out.println(
+                    "The sum of both numbers is equal to " + sum);
+            // REPEAT FOR NEXT EXERCISE
+
+        }
+}
