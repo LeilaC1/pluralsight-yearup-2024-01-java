de649c1bd26f859025a723fc06a6958a32b4334c
LeilaC1 <leilaclesca13@gmail.com>
2024-02-15 15:06:27 -0500

    phase 2 edit

--- MyCarDealership/src/main/java/com/plurasight/DealershipFileManager.java
+++ MyCarDealership/src/main/java/com/plurasight/DealershipFileManager.java
@@ -1,26 +1,8 @@
 package com.plurasight;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 
 public class DealershipFileManager {
 
-    public void database()
-    {
-        String databaseUrl = "jdbc:mysql://localhost:3306/dealership";
-        String userName = "root";
-        String password = "YU_oppdivide!20";
-         dataSource = new BasicDataSource()
-        {{
-            setUrl(databaseUrl);
-            setUsername(userName);
-            setPassword(password);
-        }};
-    }
-    public String getDealership() {
 
-    }
-    public String saveDealership(Dealership dealership) {
-        return null;
 
-    }
 }
